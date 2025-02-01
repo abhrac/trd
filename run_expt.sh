2cffcb094de563e0c6a175e34bfa1a26a67661d7
Abhra Chaudhuri <abhrachaudhuri.97@gmail.com>
2025-02-01 19:19:52 +0000

    Update run_expt.sh

--- run_expt.sh
+++ run_expt.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
-python3 src/main.py --dataset='FGVCAircraft' --seed=0 --model-type=multiview_hausdorff --train-backbone --crop-mode=random --local-weight=1e-4 --train-bsize=8 --gpu=1 --recovery-epoch=1
-python3 src/main.py --dataset='FGVCAircraft' --seed=0 --model-type=gnn_agg_hausdorff --train-backbone --crop-mode=random --local-weight=1 --train-bsize=16 --gpu=1
+# Example Training
+# python3 src/main.py --dataset='FGVCAircraft' --seed=0 --model-type=proxy_graph --train-backbone --crop-mode=random --local-weight=1 --train-bsize=8 --gpu=0 --backbone-type=disjoint_encoder
+# python3 src/main.py --dataset='FGVCAircraft' --seed=0 --model-type=multiview_hausdorff --train-backbone --crop-mode=random --local-weight=1e-4 --train-bsize=8 --gpu=1 --recovery-epoch=1
+# python3 src/main.py --dataset='FGVCAircraft' --seed=0 --model-type=gnn_agg_hausdorff --train-backbone --crop-mode=random --local-weight=1 --train-bsize=16 --gpu=1
 # python3 src/main.py --dataset='FGVCAircraft' --seed=0 --model-type=transformer_agg --train-backbone --crop-mode=random --local-weight=1
 # python3 src/main.py --dataset='FGVCAircraft' --seed=0 --pretrained
 # python3 src/main.py --dataset='StanfordCars' --seed=0
@@ -9,3 +11,10 @@ python3 src/main.py --dataset='FGVCAircraft' --seed=0 --model-type=gnn_agg_hausd
 # python3 src/main.py --dataset='iNaturalist' --seed=0 --pretrained
 # python3 src/main.py --dataset='CottonCultivar' --seed=0 --pretrained
 # python3 src/main.py --dataset='SoyCultivar' --seed=0 --pretrained
+
+# Example Evaluation
+# python3 src/main.py --dataset='FGVCAircraft' --seed=0 --model-type=proxy_graph --train-backbone --crop-mode=random --local-weight=1 --train-bsize=8 --gpu=0 --backbone-type=disjoint_encoder --pretrained --eval_only
+# python3 src/main.py --dataset='CUB' --seed=0 --model-type=proxy_graph --train-backbone --crop-mode=random --local-weight=1 --train-bsize=8 --gpu=1 --backbone-type=disjoint_encoder --pretrained --eval_only
+# python3 src/main.py --dataset='StanfordCars' --seed=0 --model-type=proxy_graph --train-backbone --crop-mode=random --local-weight=1 --train-bsize=8 --gpu=1 --backbone-type=disjoint_encoder --pretrained --eval_only
+# python3 src/main.py --dataset='CottonCultivar' --seed=0 --model-type=proxy_graph --train-backbone --crop-mode=random --local-weight=1 --train-bsize=8 --gpu=2 --backbone-type=disjoint_encoder --pretrained --eval_only
+# python3 src/main.py --dataset='SoyCultivar' --seed=0 --model-type=proxy_graph --train-backbone --crop-mode=random --local-weight=1 --train-bsize=8 --gpu=3 --backbone-type=disjoint_encoder --pretrained --eval_only
